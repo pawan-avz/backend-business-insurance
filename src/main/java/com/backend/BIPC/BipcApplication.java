e488a7d0ba91d965d2f447fabd48795afaedf810
pawan kumar <pawan.kumar@avizva.com>
2021-11-29 13:50:43 +0530

    initial commit

---
+++ src/main/java/com/backend/BIPC/BipcApplication.java
@@ -0,0 +1,13 @@
+package com.backend.BIPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BipcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/BIPC/BipcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.BIPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BipcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
